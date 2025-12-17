de86b0469121bee15db10652dba74158682468ad
MasterTM2022 <perov.public@gmail.com>
2025-12-17 10:16:28 +0300

    week 4: Order Service + improvement4

--- src/main/java/com/innowise/UserService/mapper/UserMapper.java
+++ src/main/java/com/innowise/UserService/mapper/UserMapper.java
@@ -3,10 +3,12 @@ package com.innowise.UserService.mapper;
 import com.innowise.UserService.dto.UserDto;
 import com.innowise.UserService.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "appUser", ignore = true)
     UserDto toDto(User user);
 
     User toEntity(UserDto dto);
