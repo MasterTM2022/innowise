918b2812583f9d62dd6ddb78381978d8744fee39
MasterTM2022 <perov.public@gmail.com>
2026-01-08 23:02:51 +0300

    week7: Payments.firstStep

---
+++ PaymentService/src/main/java/com/innowise/payment/entity/Payment.java
@@ -0,0 +1,44 @@
+package com.innowise.payment.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "payments", indexes = {
+        @Index(name = "idx_payments_user_id", columnList = "user_id"),
+        @Index(name = "idx_payments_status", columnList = "status"),
+        @Index(name = "idx_payments_user_status", columnList = "user_id, status"),
+        @Index(name = "uk_payments_order_id", columnList = "order_id", unique = true)
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_id", nullable = false)
+    private Long orderId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    @Column(nullable = false)
+    private Instant timestamp;
+
+    @Column(name = "payment_amount", nullable = false, precision = 19, scale = 2)
+    private BigDecimal paymentAmount;
+}
